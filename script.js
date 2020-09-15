777237bc140ff62d04a18eedbd04390b3f1641fc
SimonKana <simon.kana@seznam.cz>
2020-09-15 05:19:36 +0200

    Updated script.js, the simulation is finally working

--- script.js
+++ script.js
@@ -1,38 +1,86 @@
-let sloupce = 10;
-let radky = 10;
-let velikostPole = 40;
-let mrizka = new Array(10);
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-// Vytvoreni mrizky a zaplneni hodnotami 0 a 1
+// Rozměry herní plochy
+const rozliseni = 10;
+canvas.width = 400;
+canvas.height = 400;
+const SLOUPCE = canvas.width / rozliseni;
+const RADKY = canvas.height / rozliseni;
 
-// for (let x = 0; x < mrizka.length; x++) {
-// 	mrizka[x] = new Array(10);
-// }
+// Vytvoření 2d pole a naplnění hodnotami 0 a 1
+function vytvoritMrizku() {
+  return new Array(SLOUPCE).fill(null)
+    .map(() => new Array(RADKY).fill(null)
+      .map(() => Math.floor(Math.random() * 2)));
+}
 
-// for (let x = 0; x < sloupce; x++) {
-// 	for (let y = 0; y < radky; y++) {
-// 		mrizka[x][y] = Math.floor(Math.random() * 2);
-// 	}
-// }
+// Vytvoření herní mřížky
+let mrizka = vytvoritMrizku();
 
-window.onload = function() {
-	var can = document.getElementById('herniPlocha');
-	var ctx = can.getContext('2d');
-	
-	for (let x = 0; x < mrizka.length; x++) {
-		mrizka[x] = new Array(10);
-	}
-	
-	for (let x = 0; x < sloupce; x++) {
-		for (let y = 0; y < radky; y++) {
-			mrizka[x][y] = Math.floor(Math.random() * 2);
-			if (mrizka[x][y] == 0) {
-				ctx.fillStyle = '#fff';
-				ctx.fillRect(x*velikostPole, y*velikostPole, 40, 40);
-			} else {
-				ctx.fillStyle = '#000';
-				ctx.fillRect(x*velikostPole, y*velikostPole, 40, 40);
-			}
-		}
-	}
+// Vytvoření další generace a její vykreslení
+requestAnimationFrame(dalsiGenerace);
+
+function dalsiGenerace() {
+  mrizka = kontrolaStavuHerniPlochy(mrizka);
+  vykreslitMrizku(mrizka);
+  requestAnimationFrame(dalsiGenerace);
+}
+
+// Vykreslení mřížky na canvas
+function vykreslitMrizku(mrizka) {
+  for (let sloupec = 0; sloupec < mrizka.length; sloupec++) {
+    for (let radek = 0; radek < mrizka[sloupec].length; radek++) {
+      const bunka = mrizka[sloupec][radek];
+
+      ctx.beginPath();
+      ctx.rect(sloupec * rozliseni, radek*rozliseni, rozliseni, rozliseni);
+      ctx.fillStyle = bunka ? 'black' : 'white';
+      ctx.fill();
+      ctx.stroke();
+    }
+  }
+}
+
+// Kontrola stavu herní plochy
+function kontrolaStavuHerniPlochy(mrizka) {
+  const kopieMrizky = mrizka.map(arr => [...arr]);
+
+  for (let sloupec = 0; sloupec < mrizka.length; sloupec++) {
+    for (let radek = 0; radek < mrizka[sloupec].length; radek++) {
+      const bunka = mrizka[sloupec][radek];
+      let pocetSousednichBunek = 0;
+      for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+          if (i === 0 && j === 0) {
+            continue;
+          }
+          const x_pozice = sloupec + i;
+          const y_pozice = radek + j;
+
+          if (x_pozice >= 0 && y_pozice >= 0 && x_pozice < SLOUPCE && y_pozice < RADKY) {
+            const aktualniBunka = mrizka[x_pozice][y_pozice];
+            pocetSousednichBunek += aktualniBunka;
+          }
+        }
+      }
+
+      // Pravidla hry
+
+      if (bunka === 1 && pocetSousednichBunek < 2) {
+        // 1. Každá živá buňka s méně než dvěma živými sousedy zemře
+        kopieMrizky[sloupec][radek] = 0;
+      } else if (bunka === 1 && pocetSousednichBunek === 2 || pocetSousednichBunek === 3) {
+        // 2. Každá živá buňka se dvěma nebo třemi živými sousedy zůstává žít
+        kopieMrizky[sloupec][radek] = 1;
+      } else if (bunka === 1 && pocetSousednichBunek > 3) {
+        // 3. Každá živá buňka s více než třemi živými sousedy zemře
+        kopieMrizky[sloupec][radek] = 0;
+      } else if (bunka === 0 && pocetSousednichBunek === 3) {
+        // 4. Každá mrtvá buňka s právě třemi živými sousedy oživne
+        kopieMrizky[sloupec][radek] = 1;
+      }
+    }
+  }
+  return kopieMrizky;
 }
